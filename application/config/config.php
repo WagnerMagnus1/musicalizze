c9ae92f3536a40cf5a590bd628e6de1052ed6a13
WagnerMagnus1 <wagnerserafa@gmail.com>
2016-08-29 23:48:58 -0300

    Cadastrar ou fazer Login com face

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/olharmusical/';
+$config['base_url'] = 'http://localhost/musicalizze';
 
 /*
 |--------------------------------------------------------------------------
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'JKSDGFadsfdfjlk21315%¨&(fdsdgdsfd53s1fd';
 
 /*
 |--------------------------------------------------------------------------
